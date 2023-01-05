36030a40131e415206fec525c286804f639a7de3
Tommy Nguyen <sixty22tommy@gmail.com>
2023-01-05 12:11:29 -0800

    working

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React for 2023 
         </a>
       </header>
     </div>
